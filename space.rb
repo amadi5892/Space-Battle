9ff8ca30985bdf99e763a2307a252cbbc5dbf3c5
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-06-29 22:21:40 -0400

    First Commit

---
+++ space.rb
@@ -0,0 +1,4 @@
+'Created a text game called Space Battle!'
+'Text game works in the popup window'
+'Added a background image to the code, but background will not display'
+'tried different ways to display background image'
